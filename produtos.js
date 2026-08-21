90ba86313c0e8ab284015dbf4d23b15cbc949c28
BielzeraDeveloper <156462415+BielzeraDeveloper@users.noreply.github.com>
2026-08-21 12:12:52 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -8,26 +8,35 @@ const listaProdutos = [
     tamanhos: ["P", "M", "G", "GG", "XGG"],
     cores: ["Preta", "Verde", "Azul", "Branca"],
     imagem: "imagens/camiseta-masculina-manga-curta-algodao-pima-preta.jpeg"
-  }
-  
-  /* 
-  ==============================================================
-  COMO CADASTRAR UM NOVO PRODUTO:
-  1. Guarde a foto da peça na pasta "imagens/" do GitHub.
-  2. Remova os símbolos /* e */ desta estrutura abaixo para ativar.
-  3. Não esqueça da vírgula (,) separando cada produto!
-  ==============================================================
-
-  ,{
+  },
+  {
     id: 2,
+    titulo: "Camisa Polo Piquet Classic",
+    marca: "Baumgarten",
+    categoria: "Masculino",
+    preco: 289.90,
+    tamanhos: ["M", "G", "GG"],
+    cores: ["Marinho", "Branca"],
+    imagem: "imagens/polo-baumgarten.jpg"
+  },
+  {
+    id: 3,
+    titulo: "Jaqueta Jeans Masculina",
+    marca: "Beagle",
+    categoria: "Masculino",
+    preco: 429.00,
+    tamanhos: ["P", "M", "G", "GG"],
+    cores: ["Jeans Escuro"],
+    imagem: "imagens/jaqueta-beagle.jpg"
+  },
+  {
+    id: 4,
     titulo: "Vestido Curto Estampado",
     marca: "Farm Rio",
     categoria: "Feminino",
     preco: 349.90,
     tamanhos: ["P", "M", "G"],
     cores: ["Estampado"],
-    imagem: "imagens/nome-da-sua-foto.jpg"
+    imagem: "imagens/vestido-farm.jpg"
   }
-  */
 ];
-      
