adb98d4d4328e2950f06579067e720b6c9f0fec9
BielzeraDeveloper <156462415+BielzeraDeveloper@users.noreply.github.com>
2026-08-21 12:10:37 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -9,4 +9,25 @@ const listaProdutos = [
     cores: ["Preta", "Verde", "Azul", "Branca"],
     imagem: "imagens/camiseta-masculina-manga-curta-algodao-pima-preta.jpeg"
   }
+  
+  /* 
+  ==============================================================
+  COMO CADASTRAR UM NOVO PRODUTO:
+  1. Guarde a foto da peça na pasta "imagens/" do GitHub.
+  2. Remova os símbolos /* e */ desta estrutura abaixo para ativar.
+  3. Não esqueça da vírgula (,) separando cada produto!
+  ==============================================================
+
+  ,{
+    id: 2,
+    titulo: "Vestido Curto Estampado",
+    marca: "Farm Rio",
+    categoria: "Feminino",
+    preco: 349.90,
+    tamanhos: ["P", "M", "G"],
+    cores: ["Estampado"],
+    imagem: "imagens/nome-da-sua-foto.jpg"
+  }
+  */
 ];
+      
