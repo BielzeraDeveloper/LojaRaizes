aff85b51852f5e59cb3fb3ec6fa62b1582294a8a
BielzeraDeveloper <156462415+BielzeraDeveloper@users.noreply.github.com>
2026-08-21 11:53:15 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -1,38 +1,12 @@
-/**
- * CADASTRO DE PRODUTOS — RAÍZES MODA MASCULINA E FEMININA
- */
-
 const listaProdutos = [
   {
     id: 1,
-    titulo: "Camisa Polo Manga Curta Masculina",
-    marca: "Beagle",
-    tamanhos: ["P", "M", "G", "GG"], // Formato de lista (Array)
-    preco: 189.90,
-    imagem: "imagens/camisa1.jpg"
-  },
-  {
-    id: 2,
-    titulo: "Camisa Linho Slim Fit Premium",
+    titulo: "Camiseta Masculina Manga Curta Algodão Pima",
     marca: "Baumgarten",
-    tamanhos: ["M", "G", "GG"],
-    preco: 299.90,
-    imagem: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?w=600"
-  },
-  {
-    id: 3,
-    titulo: "Calça Jeans Slim Masculina",
-    marca: "Aurochs",
-    tamanhos: ["38", "40", "42", "44", "46"], // Aceita números de calça também!
-    preco: 229.90,
-    imagem: "imagens/calca-jeans.jpg"
-  },
-  {
-    id: 4,
-    titulo: "Vestido Midi Estampado Casual",
-    marca: "Sly Wear",
-    tamanhos: ["P", "M"],
-    preco: 249.90,
-    imagem: "https://images.unsplash.com/photo-1572804013309-59a88b7e92f1?w=600"
+    categoria: "Masculino",
+    preco: 259.10,
+    tamanhos: ["P", "M", "G", "GG", "XGG"],
+    cores: ["Preta", "Verde", "Azul", "Branca"],
+    imagem: "foto-camiseta-pima.jpg"
   }
 ];
