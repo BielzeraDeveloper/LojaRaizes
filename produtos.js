b308ae8b0b89843950f314c4cbd1b00a058efc8b
BielzeraDeveloper <156462415+BielzeraDeveloper@users.noreply.github.com>
2026-08-22 10:35:54 -0300

    Adiciona RAIZ-1432 com preço R$ 109.99

--- produtos.js
+++ produtos.js
@@ -1,5 +1,21 @@
 const listaProdutos = [
   {
+    "id": 1787405753884,
+    "codigo": "RAIZ-1432",
+    "titulo": "Boné ",
+    "marca": "Beagle",
+    "preco": 109.99,
+    "imagem": "imagens/RAIZ-1432.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Padrão"
+    ]
+  },
+  {
     "id": 1787404204233,
     "codigo": "RAIZ-8049",
     "titulo": "Camisa Slim Poliamida",
