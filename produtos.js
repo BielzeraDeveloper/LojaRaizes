4db45c39edfc4ddcad77cf2097dc9879b6372da5
BielzeraDeveloper <156462415+BielzeraDeveloper@users.noreply.github.com>
2026-08-22 10:10:05 -0300

    Adiciona RAIZ-8049 com preço R$ 263.99

--- produtos.js
+++ produtos.js
@@ -1,22 +1,50 @@
 const listaProdutos = [
   {
-    id: 1,
-    codigo: "RAIZ-1001",
-    titulo: "Camiseta Linho Premium",
-    marca: "Beagle",
-    preco: 189.90,
-    imagem: "imagens/RAIZ-1001.jpg",
-    tamanhos: ["P", "M", "G"],
-    cores: ["Preto", "Branco"]
+    "id": 1787404204233,
+    "codigo": "RAIZ-8049",
+    "titulo": "Camisa Slim Poliamida",
+    "marca": "SOUL",
+    "preco": 263.99,
+    "imagem": "imagens/RAIZ-8049.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Padrão"
+    ]
   },
   {
-    id: 2,
-    codigo: "RAIZ-1002",
-    titulo: "Vestido Estampado Floral",
-    marca: "Farm Rio",
-    preco: 299.90,
-    imagem: "imagens/RAIZ-1002.jpg",
-    tamanhos: ["P", "M"],
-    cores: ["Multicolor"]
+    "id": 1,
+    "codigo": "RAIZ-1001",
+    "titulo": "Camiseta Linho Premium",
+    "marca": "Beagle",
+    "preco": 189.9,
+    "imagem": "imagens/RAIZ-1001.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Preto",
+      "Branco"
+    ]
+  },
+  {
+    "id": 2,
+    "codigo": "RAIZ-1002",
+    "titulo": "Vestido Estampado Floral",
+    "marca": "Farm Rio",
+    "preco": 299.9,
+    "imagem": "imagens/RAIZ-1002.jpg",
+    "tamanhos": [
+      "P",
+      "M"
+    ],
+    "cores": [
+      "Multicolor"
+    ]
   }
 ];
