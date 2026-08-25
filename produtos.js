ca8ff77c720bceffd66142fb254a2410d40d6ca6
BielzeraDeveloper <156462415+BielzeraDeveloper@users.noreply.github.com>
2026-08-25 12:28:42 -0300

    Adiciona RAIZ-8367 com preço R$ 164.99

--- produtos.js
+++ produtos.js
@@ -1,5 +1,21 @@
 const listaProdutos = [
   {
+    "id": 1787671721327,
+    "codigo": "RAIZ-8367",
+    "titulo": "Camiseta",
+    "marca": "Soul",
+    "preco": 164.99,
+    "imagem": "imagens/RAIZ-8367.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Padrão"
+    ]
+  },
+  {
     "id": 1787405753884,
     "codigo": "RAIZ-1432",
     "titulo": "Boné ",
