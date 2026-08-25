fd41e293190b0e494d315397d987bb1998352707
BielzeraDeveloper <156462415+BielzeraDeveloper@users.noreply.github.com>
2026-08-25 13:04:24 -0300

    Adiciona RAIZ-8451 com preço R$ 197.99

--- produtos.js
+++ produtos.js
@@ -1,4 +1,82 @@
 const listaProdutos = [
   {
+    "id": 1787673863844,
+    "codigo": "RAIZ-8451",
+    "titulo": "Camiseta Masculina",
+    "marca": "Soul",
+    "preco": 197.99,
+    "imagem": "imagens/RAIZ-8451.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Padrão"
+    ]
+  },
+  {
+    "id": 1787672777584,
+    "codigo": "RAIZ-5105",
+    "titulo": "Camiseta Masculina ",
+    "marca": "Soul",
+    "preco": 197.99,
+    "imagem": "imagens/RAIZ-5105.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Padrão"
+    ]
+  },
+  {
+    "id": 1787671721327,
+    "codigo": "RAIZ-8367",
+    "titulo": "Camiseta",
+    "marca": "Soul",
+    "preco": 164.99,
+    "imagem": "imagens/RAIZ-8367.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Padrão"
+    ]
+  },
+  {
+    "id": 1787405753884,
+    "codigo": "RAIZ-1432",
+    "titulo": "Boné ",
+    "marca": "Beagle",
+    "preco": 109.99,
+    "imagem": "imagens/RAIZ-1432.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Padrão"
+    ]
+  },
+  {
+    "id": 1787404204233,
+    "codigo": "RAIZ-8049",
+    "titulo": "Camisa Slim Poliamida",
+    "marca": "SOUL",
+    "preco": 263.99,
+    "imagem": "imagens/RAIZ-8049.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Padrão"
+    ]
   }
 ];
