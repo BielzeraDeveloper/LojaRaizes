5e96693dcb390ba2237254d79a19b3feda9a6b68
BielzeraDeveloper <156462415+BielzeraDeveloper@users.noreply.github.com>
2026-08-26 09:41:09 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -1,34 +1,146 @@
 const listaProdutos = [
   {
-    "id": 1787673997174,
-    "codigo": "RAIZ-2623",
-    "titulo": "Camiseta Masculina",
+    "id": 1787673997101,
+    "codigo": "BB",
+    "titulo": "Boné",
     "marca": "Beagle",
-    "preco": 219.99,
-    "imagem": "imagens/RAIZ-2623.jpg",
+    "preco": 139.99,
+    "imagem": "imagens/bb.jpg",
     "tamanhos": [
       "P",
       "M",
       "G"
     ],
     "cores": [
-      "Padrão"
+      "Cores"
     ]
   },
   {
-    "id": 1787672777584,
-    "codigo": "RAIZ-5105",
-    "titulo": "Camiseta Masculina ",
-    "marca": "Soul",
-    "preco": 197.99,
-    "imagem": "imagens/RAIZ-5105.jpg",
+    "id": 1787673997102,
+    "codigo": "BB2",
+    "titulo": "Boné",
+    "marca": "Beagle",
+    "preco": 109.99,
+    "imagem": "imagens/bb2.jpg",
     "tamanhos": [
       "P",
       "M",
       "G"
     ],
     "cores": [
-      "Padrão"
+      "Cores"
+    ]
+  },
+  {
+    "id": 1787673997103,
+    "codigo": "CFB",
+    "titulo": "Camiseta Feminina",
+    "marca": "Baumgarten",
+    "preco": 165.99,
+    "imagem": "imagens/cfb.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Cores"
+    ]
+  },
+  {
+    "id": 1787673997104,
+    "codigo": "CFB2",
+    "titulo": "Camiseta Feminina",
+    "marca": "Baumgarten",
+    "preco": 119.99,
+    "imagem": "imagens/cfb2.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Cores"
+    ]
+  },
+  {
+    "id": 1787673997105,
+    "codigo": "CJMB",
+    "titulo": "Calça Jeans Masculina",
+    "marca": "Beagle",
+    "preco": 329.99,
+    "imagem": "imagens/cjmb.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Cores"
+    ]
+  },
+  {
+    "id": 1787673997106,
+    "codigo": "CMB",
+    "titulo": "Camiseta Masculina",
+    "marca": "Beagle",
+    "preco": 219.99,
+    "imagem": "imagens/cmb.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Cores"
+    ]
+  },
+  {
+    "id": 1787673997107,
+    "codigo": "CMBA",
+    "titulo": "Camiseta Masculina Polo",
+    "marca": "Baumgarten",
+    "preco": 179.99,
+    "imagem": "imagens/cmba.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Cores"
+    ]
+  },
+  {
+    "id": 1787673997108,
+    "codigo": "CMBP",
+    "titulo": "Camiseta Masculina Polo",
+    "marca": "Beagle",
+    "preco": 329.99,
+    "imagem": "imagens/cmbp.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Cores"
+    ]
+  },
+  {
+    "id": 1787673997109,
+    "codigo": "CMS",
+    "titulo": "Camiseta Masculina",
+    "marca": "Soul",
+    "preco": 197.99,
+    "imagem": "imagens/cms.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G"
+    ],
+    "cores": [
+      "Cores"
     ]
   }
 ];
