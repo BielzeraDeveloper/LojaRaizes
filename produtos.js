4c71fee6ec8ada2be8ea5c845592669deef48c57
BielzeraDeveloper <156462415+BielzeraDeveloper@users.noreply.github.com>
2026-09-02 12:53:39 -0300

    Adiciona RAIZ-8575 com preço R$ 119.99

--- produtos.js
+++ produtos.js
@@ -1,5 +1,22 @@
 const listaProdutos = [
   {
+    "id": 1788364953710,
+    "codigo": "RAIZ-8575",
+    "titulo": "Camiseta Masculina",
+    "marca": "Baumgarten",
+    "preco": 119.99,
+    "imagem": "imagens/RAIZ-8575.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "G",
+      "GG"
+    ],
+    "cores": [
+      "Padrão"
+    ]
+  },
+  {
     "id": 1788364715017,
     "codigo": "RAIZ-7133",
     "titulo": "Camiseta Masculina",
