5dbdd159d99967f863a90c934268b4441b62319e
BielzeraDeveloper <156462415+BielzeraDeveloper@users.noreply.github.com>
2026-09-19 10:07:00 -0300

    Atualiza produto RAIZ-5885

--- produtos.js
+++ produtos.js
@@ -30,7 +30,7 @@ const listaProdutos = [
   {
     "id": 1789822558215,
     "codigo": "RAIZ-5885",
-    "titulo": "Vestido Curto Boho de Mar",
+    "titulo": "Vestido Curto Boho de Mar Fem",
     "marca": "FarmRio",
     "preco": 398,
     "imagem": "imagens/RAIZ-5885.jpg",
