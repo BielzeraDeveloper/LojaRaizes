520cd4c2386cecef53b746ef1e04a0ecad6c6592
BielzeraDeveloper <156462415+BielzeraDeveloper@users.noreply.github.com>
2026-09-22 14:32:53 -0300

    Atualiza produto RAIZ-2711

--- produtos.js
+++ produtos.js
@@ -2,7 +2,7 @@ const listaProdutos = [
   {
     "id": 1790098295619,
     "codigo": "RAIZ-2711",
-    "titulo": "Shorts jeans",
+    "titulo": "Shorts jeans Feminino",
     "marca": "SlyWear",
     "preco": 399.9,
     "imagem": "imagens/RAIZ-2711.jpg",
