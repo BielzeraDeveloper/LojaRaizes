f12f96b89f3cc976e472c56b98a4752db12e3d45
BielzeraDeveloper <156462415+BielzeraDeveloper@users.noreply.github.com>
2026-09-23 15:33:57 -0300

    Adiciona RAIZ-6801 com preço R$ 279.99

--- produtos.js
+++ produtos.js
@@ -1,5 +1,19 @@
 const listaProdutos = [
   {
+    "id": 1790188437318,
+    "codigo": "RAIZ-6801",
+    "titulo": "Camisa Masculina Manga Longa",
+    "marca": "Aurochs",
+    "preco": 279.99,
+    "imagem": "imagens/RAIZ-6801.jpg",
+    "tamanhos": [
+      "M"
+    ],
+    "cores": [
+      "Padrão"
+    ]
+  },
+  {
     "id": 1790188364479,
     "codigo": "RAIZ-7722",
     "titulo": "Camisa Masculina Manga Longa",
