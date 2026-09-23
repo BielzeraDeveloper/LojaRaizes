bcea743451014e098902e0f855a8f1bc055a84c5
BielzeraDeveloper <156462415+BielzeraDeveloper@users.noreply.github.com>
2026-09-23 11:00:44 -0300

    Atualiza produto RAIZ-7831

--- produtos.js
+++ produtos.js
@@ -16,7 +16,7 @@ const listaProdutos = [
   {
     "id": 1790109088071,
     "codigo": "RAIZ-7831",
-    "titulo": "Vestido longo céu de arara",
+    "titulo": "Vestido Longo Céu de Arara",
     "marca": "Farm rio",
     "preco": 598,
     "imagem": "imagens/RAIZ-7831.jpg",
