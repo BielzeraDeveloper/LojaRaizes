4c7496fe1df04a23f1f7ef7414b40ec805f2fce5
BielzeraDeveloper <156462415+BielzeraDeveloper@users.noreply.github.com>
2026-09-23 14:35:14 -0300

    Adiciona RAIZ-3243 com preço R$ 279.99

--- produtos.js
+++ produtos.js
@@ -1,5 +1,21 @@
 const listaProdutos = [
   {
+    "id": 1790184914451,
+    "codigo": "RAIZ-3243",
+    "titulo": "Camisa Xadrez Masculina  Manga Longa",
+    "marca": "Aurochs",
+    "preco": 279.99,
+    "imagem": "imagens/RAIZ-3243.jpg",
+    "tamanhos": [
+      "P",
+      "M",
+      "GG"
+    ],
+    "cores": [
+      "Padrão"
+    ]
+  },
+  {
     "id": 1790184801624,
     "codigo": "RAIZ-9563",
     "titulo": "Camisa Xadrez Masculina",
